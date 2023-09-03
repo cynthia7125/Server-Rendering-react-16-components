c8a670ddd5b9984bd6274e9d7bbfdc0df1279ff5
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-03 12:01:07 +0300

    Add functionality

--- server/index.js
+++ server/index.js
@@ -1,22 +1,80 @@
 // const express = require('express');
-import { readFileSync } from 'fs';
-import express from 'express';
-import React from 'react';
-import { App } from '../client/App'
-import { renderToString } from 'react-dom/server'
+import { readFileSync } from "fs";
+import express from "express";
+import React from "react";
+import { App } from "../client/App";
+import { renderToString } from "react-dom/server";
+
+const data = {
+  questions: [
+    {
+      questionId: "Q3",
+      constent: "Should we use JQuery or Fetch for Ajax?",
+    },
+    {
+      questionId: "Q1",
+      content: "Which back end solution should we use for our application?",
+    },
+    {
+      questionId: "Q2",
+      content:
+        "What percentage of developer time should be devoted to end-to-end testing?",
+    },
+  ],
+  answers: [
+    {
+      answerId: "A7",
+      questionID: "Q3",
+      upvotes: 2,
+      content: "JQuery",
+    },
+    {
+      answerId: "A1",
+      questionId: 1,
+      upvotes: 2,
+      content: "Apache",
+    },
+    {
+      answerId: "A2",
+      questionId: "Q1",
+      upvotes: 0,
+      content: "Java",
+    },
+    {
+      answerId: "A3",
+      questionId: "Q1",
+      upvotes: 4,
+      content: "Node.js",
+    },
+    {
+      answerId: "A4",
+      questionId: "Q2",
+      upvotes: 2,
+      content: "25%",
+    },
+    {
+      answerId: "A5",
+      questionId: "Q2",
+      upvotes: 1,
+      content: "50%",
+    },
+    {
+      answerId: "A6",
+      questionId: "Q2",
+      upvotes: 1,
+      content: "75%",
+    },
+  ],
+};
 
 const app = new express();
 
 app.use(express.static("dist"));
 
-app.get('/', async (_req, res) => {
-
-    const index = readFileSync(`public/index.html`, `utf8`);
-    const rendered = renderToString(<App />)
-    res.send(index.replace("{{rendered}}", rendered));
-    // res.send(
-    //     `<h1>REACT IS EXCELLENT</h1`
-    // )
+app.get("/", async (_req, res) => {
+  const index = readFileSync(`public/index.html`, `utf8`);
+  const rendered = renderToString(<App {...data}/>);
+  res.send(index.replace("{{rendered}}", rendered));
 });
 
 app.listen(5500);
